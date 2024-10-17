8e69405e9ade059229585be4fc0bc9c765e44010
Maddy <madeline.kovaleski@gmail.com>
2024-10-17 12:41:58 -0700

    final draft

--- hw2.2.py
+++ hw2.2.py
@@ -6,7 +6,10 @@ starting_epsilon = 0.1
 tol = 1e-4 # define a tolerance level
 col = ['r', 'b', 'g', 'c', 'm', 'k'] # eigenfunc colors
 yp = [-4, 4]
-x = np.linspace(yp[0], yp[1],8000)
+x = np.linspace(yp[0], yp[1],81, endpoint=True)
+A1 = np.zeros((81,5))
+A2 = np.zeros(5)
+
 
 def system(y, x, epsilon):
     dy1dx = y[1]
@@ -27,8 +30,12 @@ for modes in range(1, 6): # begin mode loop
         else:
             epsilon -= depsilon / 2
             depsilon /= 2
+    A2[modes-1] = epsilon
     starting_epsilon = epsilon + 0.1 # after finding eigenvalue, pick new start
     norm = np.trapz(y[:, 0] * y[:, 0], x) # calculate the normalization
+    A1[:,modes-1] = np.abs(y[:, 0] / np.sqrt(norm))
     plt.plot(x, y[:, 0] / np.sqrt(norm), col[modes - 1]) # plot modes
 
 plt.show()
+print(A1)
+print(A2)
