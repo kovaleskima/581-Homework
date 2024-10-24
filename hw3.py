6bf7fa55786f6153598f9387997e382d7d8caee7
Maddy <madeline.kovaleski@gmail.com>
2024-10-24 16:00:24 -0700

    constructed A

--- hw3.py
+++ hw3.py
@@ -1,20 +1,26 @@
 import numpy as np
 from scipy.integrate import solve_bvp
+from scipy.sparse import csr_matrix
 import matplotlib.pyplot as plt
 
-def bvp_rhs2(x, y, epsilon):
-    return np.vstack((y[1], (x**2 - epsilon) * y[0]))
-
-def bvp_bc2(yl, yr, epsilon):
-    print(epsilon, type(epsilon))
-    return np.array([yl[0], yl[0]*np.sqrt(16-float(epsilon)), yr[0], -yr[0]*np.sqrt(16-float(epsilon))])
-
-def mat4init(x):
-    return np.array([np.cos((np.pi/2)*x),-(np.pi / 2)*np.sin((np.pi/2)*x)])
-
 epsilon = 0.1
-x_init = np.linspace(-4, 4, 80)
-init2 = solve_bvp(bvp_rhs2, bvp_bc2, x_init, mat4init(x_init), p=[epsilon])
-x2 = np.linspace(-1, 1, 100)
-BS2 = init2.sol(x2)
-plt.plot(x2, BS2[0])
+x = np.linspace(-4, 4, 81)
+x_int = np.linspace(-3.9, 3.9, 79)
+dx = 0.1
+n = 79
+
+A = np.zeros((n,n))
+A[0][0] = (-2-(x_int[0]**2)*(dx**2)) + (4/3)
+A[0][1] = (2/3)
+A[n-1][n-1] = (-2-(x_int[n-1]**2)*(dx**2)) + (4/3)
+A[n-1][n-2] = (2/3)
+
+for j in range(1, n-2):
+    A[j][j] = (-2-(x_int[j]**2)*(dx**2))
+    A[j][j-1] = 1
+    A[j][j+1] = 1
+    
+A_sparse = csr_matrix(A)
+        
+
+print(A)
